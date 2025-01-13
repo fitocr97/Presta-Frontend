66371a5d0dac2a4aed70640e73ca75668fe617b3
Rodolfo Mora <rodolfomora97@outlook.com>
2025-01-12 21:23:14 -0600

    url api

--- js/fetchBalance.js
+++ js/fetchBalance.js
@@ -7,7 +7,7 @@ const fetchTotalBalance = async () => {
             return;
         }
 
-        const response = await axios.get('http://localhost:3000/api/accounts/balance', {
+        const response = await axios.get('https://presta-backend-production.up.railway.app/prestaapi/v1/accounts/balances', {
             headers: {
                 'Authorization': `Bearer ${token}`, // Pasar el Bearer Token en el header
                 'Content-Type': 'application/json', // Asegurar que el Content-Type sea JSON
